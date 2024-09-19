dcbb845faf8e270629a52287eca656b28e2be18c
PabloTemprano <pablo.temprano@estudiantat.upc.edu>
2024-09-19 13:41:26 +0200

    Update Problema3.R

--- Problema3.R
+++ Problema3.R
@@ -1,1 +1,9 @@
-#Codigo para problema 3
+#codigo para problema 3:
+#o si es una lista de numeros como en el parcial hay que poner c(numeros)
+x <- (1:30)/30
+sum(x)
+y <- x[-c(21,2,26)]
+y
+sum(exp(x))-sum(exp(y))
+y[19]
+y[24]
